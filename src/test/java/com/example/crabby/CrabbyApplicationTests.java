931f6bd16aa80a70fbb176169642e59ac1a62c6a
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-10 10:46:32 -0500

    first commit

---
+++ src/test/java/com/example/crabby/CrabbyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crabby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrabbyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
